e61d1c2df16bd782f7990b56365c754f25755076
Jacob Westerback <sjc5283@g.rit.edu>
2017-04-16 00:46:20 -0400

    Added in jquery locally, streamlined card generater, added in fraction generator, fixed visuals for drag and drop

--- activities/Fractionator.activity/js/script.js
+++ activities/Fractionator.activity/js/script.js
@@ -1,6 +1,31 @@
 var adjustment;
+var denominators = [1,2,3,4,5,6,8,10,12,100]
+var possible = [170];
+var names = [170];
+
+function Fraction (index) {
+    this.value = possible[index];
+    this.fraction = names[index];
+	this.numerator = this.fraction.split("/")[0];
+	this.denominator = this.fraction.split("/")[1];
+}
+
+function makeFractions() {
+	count = 0;
+	for (i = 0; i < denominators.length; i++) {
+		for (j = 0; j <= denominators[i]; j++) {
+			if(denominators[i] != 100 || Math.random() < 0.25) {
+				names[count]=""+j+"/"+denominators[i];
+				possible[count] = j/denominators[i];
+				count++;
+			}
+		} 
+	}
+}
 
 $(document).ready(function() { 
+	makeFractions();
+
 	$("#start").on("click", function(){
 		$("#menu").css("display", "none");
 		$("#game").css("display", "block");
@@ -25,62 +50,61 @@ $(document).ready(function() {
 		setUpGame();
 	});	
 	
-	$("ol.simple_with_animation").sortable({	
-	  group: 'simple_with_animation',
-	  pullPlaceholder: false,
-	  // animation on drop
-	  onDrop: function  ($item, container, _super) {
-		var $clonedItem = $('<li/>').css({height: 0});
-		$item.before($clonedItem);
-		$clonedItem.animate({'height': $item.height()});
-
-		$item.animate($clonedItem.position(), function  () {
-		  $clonedItem.detach();
-		  _super($item, container);
-		});
-	  },
-
-	  // set $item relative to cursor position
-	  onDragStart: function ($item, container, _super) {
-		var offset = $item.offset(),
-			pointer = container.rootGroup.pointer;
-
-		adjustment = {
-		  left: pointer.left - offset.left,
-		  top: pointer.top - offset.top
-		};
-
-		_super($item, container);
-	  },
-	  onDrag: function ($item, position) {
-		$item.css({
-		  left: position.left - adjustment.left,
-		  top: position.top - adjustment.top
-		});
-	  }
-	});
+    $("#cardList" ).sortable();
+    $("#cardList" ).disableSelection();
 });
 
 function setUpGame() {
     var difficulty = $('input[name=difficulty]:checked').val();
     var amount = $('input[name=amount]:checked').val();
 	
-	document.getElementById("cardList").innerHTML = "<li>1</li><li>2</li><li>3</li>";
+	var newItemsHTML = "";
+	var amt = 0;
+	var val = "";
 		
-	if (amount != "small") {
-		var newItemsHTML = "";
-		if (amount == "med") {
-			for (i = 0; i < 3; i++) { 
-				newItemsHTML += "<li>"+(i+4)+"</li>";
-			}
-		}
-		else if (amount == "large") {
-			for (i = 0; i < 9; i++) { 
-				newItemsHTML += "<li>"+(i+4)+"</li>";
-			} 
-		}
-		document.getElementById("cardList").innerHTML += newItemsHTML;
+	switch (amount){
+		case "small":
+			amt = 3;
+			break;
+		case "med":
+			amt = 6;
+			break;
+		case "large":
+			amt = 12;
+			break;
+		default:
+			break;
 	}
+	
+	var fractions = randomFractions(amt);
+		
+	for (i = 0; i < amt; i++) { 
+		val = fractions[i].value;
+		numerator = fractions[i].numerator;
+		denominator = fractions[i].denominator;
+		newItemsHTML += "<li><p><span class=\"value\">"+val+"</span><span class=\"frac\"><sup>"+numerator+"</sup><span>/</span><sub>"+denominator+"</sub></span></p></li>";
+	} 
+	
+	document.getElementById("cardList").innerHTML = newItemsHTML;
+}
+
+function randomFractions (amount) {
+	var fractions = [amount];
+	for (i = 0; i < amount; i++) { 
+		fractions[i] = randomFraction();
+	}
+	return fractions;
+}
+
+function randomFraction() {
+	var rand = getRandomInt(0,possible.length);
+	return new Fraction(rand);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function check() {
